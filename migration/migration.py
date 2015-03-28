42f9f8b3ce5b5c7c2336014bb756fd9b4ee2ba85
agita <agita>
2015-03-29 00:12:21 +0300

    migration fixes

--- migration/migration.py
+++ migration/migration.py
@@ -3,59 +3,58 @@ import sys
 import struct
 import re
 import psycopg2
-import binascii
 
+print 'Initialize db connection: Start'
 conn = psycopg2.connect(
 	host = '127.0.0.1',
 	database = 'scapack',
 	user = 'postgres',
 	password = 'postgres'
 )
-
 cursor = conn.cursor()
+print 'Initialize db connection: Done'
 
-for i in range(len(os.listdir(sys.argv[1]))):
-    fileName = os.listdir(sys.argv[1])[i]
-    if os.listdir(sys.argv[1])[i] != 'tmp_file.tmp':
-        tmp_file = open(sys.argv[1] + 'tmp_file.tmp', 'w+')
-        pathToTrace = sys.argv[1] + "/" + fileName
-        lines = open(pathToTrace, "r").read().splitlines()
+lenDir = len(os.listdir(sys.argv[1]))
+listDir = os.listdir(sys.argv[1])
+print 'Files for migration: ', lenDir
 
-        del lines[0:24]
-        len_lines = len(lines)
+for i in range(lenDir):
 
-        for j in range(len_lines):
-            lines[j] = int(lines[j])
-        len_lines = len(lines)
+    fileName = listDir[i]
 
-        buffer = struct.pack('i'*len_lines, *lines)
-        print buffer.encode('hex_codec')
-        """
-        tmp_file.seek(0)
-        tmp_file.truncate()
-        tmp_file.write(binascii.hexlify(buffer))
-        """
-        key = re.search('(?<=k=)(.*)(?=_m)', fileName).group(0)
-        message = re.search('(?<=m=)(.*)(?=_c)', fileName).group(0)
-        cipher = re.search('(?<=c=)(.*)(?=\.)', fileName).group(0)
+    #tmp_file = open(sys.argv[1] + 'tmp_file.tmp', 'w+')
+    pathToTrace = sys.argv[1] + "/" + fileName
+    lines = open(pathToTrace, "r").read().splitlines()
 
-        query = "INSERT INTO trace (name, data, key, message, cipher) VALUES (%s,  %s, %s, %s, %s);"
-        content = (fileName, buffer.encode('hex_codec'), key, message, cipher)
+    del lines[0:24]
+    len_lines = len(lines)
 
-        tmp_file.close()
+    for j in range(len_lines):
+        lines[j] = int(lines[j])
+    len_lines = len(lines)
 
-        cursor.execute(query, content)
+    buffer = struct.pack('i'*len_lines, *lines)
+    key = re.search('(?<=k=)(.*)(?=_m)', fileName).group(0)
+    message = re.search('(?<=m=)(.*)(?=_c)', fileName).group(0)
+    cipher = re.search('(?<=c=)(.*)(?=\.)', fileName).group(0)
 
-        conn.commit()
+    print 'File number: ', i, '/', lenDir
 
-        print '------------------------------------------------'
-        print 'key: ', key
-        print 'message: ', message
-        print 'cipher: ', cipher
-        print fileName + '\nDone'
-        print '------------------------------------------------'
-        print cursor.execute('select data from trace;')
-    """
+    query = "INSERT INTO trace (name, data, key, message, cipher) VALUES (%s,  %s, %s, %s, %s);"
+    content = (fileName, buffer.encode('hex_codec'), key, message, cipher)
+
+    cursor.execute(query, content)
+
+    print '------------------------------------------------'
+    print 'key: ', key
+    print 'message: ', message
+    print 'cipher: ', cipher
+    print fileName + '\nDone'
+
+conn.commit()
+print 'COMMIT IT!'
+
+"""
     for j in range(len(lines)):
         lines[j] = "{0:b}".format(int(lines[j]))
         #print lines[j]
@@ -64,16 +63,13 @@ for i in range(len(os.listdir(sys.argv[1]))):
     #[int(m) for m in lines]
     #for t in range (30):
     #    print
-    """
-    """
+
     with open(pathToTrace, "r") as tmpTraceFile:
         tmpArray = tmpTraceFile.readlines()
         tmpTrace = tmpTraceFile.read()
         #lines = (line.rstrip('\n') for line in open(pathToTrace, "r"))
         lines = open(tmpTraceFile).read().splitlines()
     tmpTraceFile.close()
-    """
-    """
 for i in range (24):
     print lines[i]
-    """
+"""
